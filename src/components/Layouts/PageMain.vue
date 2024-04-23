0643106e05397d7746feb93634e88cebfad14c7f
Adryel Dearo <adryelzd77@outlook.com>
2024-04-23 18:17:11 -0300

    Refatorando os componentes

--- src/components/Layouts/PageMain.vue
+++ src/components/Layouts/PageMain.vue
@@ -1,7 +1,10 @@
 <script setup>
   import { ref, computed } from 'vue';
-  import PokemonCard from '../PokemonList/PokemonCard.vue';
   import { usePokeApi } from '../../services/api.js';
+  import Card from '../PokemonList/Card.vue';
+  import ErrorMessage from '../App/ErrorMessage.vue';
+  import Filters from '../PokemonList/Filters.vue';
+
 
   const baseUrl = 'https://pokeapi.co/api/v2/pokemon?limit='
   const limit = ref(20);
@@ -11,30 +14,38 @@
   const { data, error } = usePokeApi(url);
 
   const pokemons = computed(() => {
-  if(data.value && search.value){
-    return data.value.results.filter(pokemon => {
-      return pokemon.name.includes(search.value.toLowerCase());
-    })
-  }
+    if(data.value && search.value){
+      return data.value.results.filter(pokemon => {
+        return pokemon.name.includes(search.value.toLowerCase());
+      })
+    }
 
-  return data.value ? data.value.results : [];
-})
+    return data.value ? data.value.results : [];
+  })
 </script>
 
 <template>
   <main class="flex items-center flex-col w-9/12 mx-auto">
-    <div class="p-2">
-      <label for="limit">Limite</label>
-      <input v-model="limit" class="mx-2 p-1 rounded" type="text" name="limit" :placeholder="limit">
-    
-      <label for="name">Nome</label>
-      <input v-model="search" class="mx-2 p-1 rounded" type="text" name="name">
+    <Filters 
+      :limit="limit"
+      :search="search"
+      @update:limit="limit = $event"
+      @update:search="search = $event"/>
+
+    <div class="my-4">
+      <h1 class="text-2xl font-bold">Pokemons</h1>
     </div>
-    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      <PokemonCard
+
+    <ErrorMessage 
+      v-if="error"
+      :message = "error"/>
+    <p v-else-if="!data">Carregando...</p>
+    <div v-else class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <Card
         v-for="pokemon in pokemons"
         :name="pokemon.name"
-        :pokemonCode="pokemon.url.split('/')[6]"/>
+        :pokemonCode="pokemon.url.split('/')[6]"
+        :id="pokemon.id"/>
     </div>
   </main>
 </template>
---
+++ src/components/PokemonList/Filters.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import { ref, watch } from 'vue';
+  const props = defineProps(['limit', 'search']);
+  const emit = defineEmits(['update:limit', 'update:search']);
+
+  const limit = ref(props.limit);
+  const search = ref(props.search);
+
+  watch(limit, (newVal) => {
+    emit('update:limit', newVal);
+  });
+
+  watch(search, (newVal) => {
+    emit('update:search', newVal);
+  });
+</script>
+
+<template>
+  <div class="flex items-center justify-between p-2">
+    <div>
+      <label for="limit">Limite</label>
+      <input v-model="limit" class="mx-2 p-1 rounded" type="text" name="limit" :placeholder="props.limit">
+    </div>
+    <div>
+      <label for="name">Nome</label>
+      <input v-model="search" class="mx-2 p-1 rounded" type="text" name="name">
+    </div>
+  </div>
+</template>
