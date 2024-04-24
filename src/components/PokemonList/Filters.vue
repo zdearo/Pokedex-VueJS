553c638d682e84773f46bb292a70a89e4352be73
Adryel Dearo <adryelzd77@outlook.com>
2024-04-24 17:41:04 -0300

    Corringindo a pesquisa, agora é feita entre todos os pokemon

--- src/components/PokemonList/Filters.vue
+++ src/components/PokemonList/Filters.vue
@@ -1,12 +1,20 @@
 <script setup>
+  import { ref } from 'vue';
+
   const search = defineModel();
+  const tempSearch = ref('');
+
+  const updateSearch = () => {
+    search.value = tempSearch.value;
+  };
 </script>
 
 <template>
   <div class="flex items-center justify-between p-2">
     <div>
       <label for="name">Nome</label>
-      <input v-model="search" class="mx-2 p-1 rounded" type="text" name="name">
+      <input v-model="tempSearch" @keydown.enter="updateSearch" class="mx-2 p-1 rounded" type="text" name="name">
+      <button class="px-2 py-1 text-white bg-blue-500 rounded-md" @click="updateSearch">Pesquisar</button>
     </div>
   </div>
 </template>
